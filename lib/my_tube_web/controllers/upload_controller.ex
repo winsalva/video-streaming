9e90060fccdc203fbc62bfab21b48fc725a6701e
* <winsalva396@gmail.com>
2021-03-14 12:15:46 +0800

    Bug fixing

--- lib/my_tube_web/controllers/upload_controller.ex
+++ lib/my_tube_web/controllers/upload_controller.ex
@@ -12,43 +12,6 @@ defmodule MyTubeWeb.UploadController do
   end
 
 
-  @doc """
-  Temporary disabling this part of code until such time i can make it to host file uploads. Will just embed youtube videos for now.
-
-  def create(conn, %{"upload" => %{"title" => title, "file" => file, "description" => description} = _params}) do
-    user_id = conn.assigns.current_user.id
-    
-    params = %{
-      user_id: user_id,
-      title: title,
-      file: file.filename,
-      description: description
-    }
-    case Upload.insert_upload(file, user_id, params) do
-      {:ok, upload} ->
-        conn
-	|> put_flash(:info, "Upload successfully")
-	|> redirect(to: "/")
-      {:error, %Ecto.Changeset{} = changeset} ->
-        conn
-	|> render(:new, changeset: changeset)
-    end
-  end
-
-
-  def create(conn, %{"upload" => %{"title" => title, "description" => description} = params}) do
-    case Upload.insert_upload(params) do
-      {:error, %Ecto.Changeset{} = changeset} ->
-        conn
-	|> render(:new, changeset: changeset)
-      _ ->
-        conn
-	|> put_flash(:error, "There was an error uploading your video.")
-	|> redirect(to: "/")
-    end
-  end
-  """
-
 
   def create(conn, %{"upload" => %{"title" => title, "file" => file, "description" => description} = params}) do
     params = Map.put(params, "user_id", conn.assigns.current_user.id)
