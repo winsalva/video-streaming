7dc6664f2e21cc4b6ff31eadf450cfcac457576b
* <winsalva396@gmail.com>
2021-03-14 11:51:53 +0800

    Fix uploads

--- lib/my_tube_web/global_helpers.ex
+++ lib/my_tube_web/global_helpers.ex
@@ -3,4 +3,15 @@ defmodule MyTubeWeb.GlobalHelpers do
   def count(list) do
     length(list)
   end
+
+
+  def url(url) do
+    yt_link = "https://youtube.com/embed/"
+    video_id =
+      url
+      |> String.split("/")
+      |> List.last()
+
+    yt_link <> video_id
+  end
 end
